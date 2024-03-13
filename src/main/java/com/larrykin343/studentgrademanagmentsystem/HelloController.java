1be92247cfc5c4c184ef15b91c73c257c537e277
Kinuthia Lawrence <kinuthialawrence343@gmail.com>
2024-03-13 07:36:32 +0300

    initial commit

---
+++ src/main/java/com/larrykin343/studentgrademanagmentsystem/HelloController.java
@@ -0,0 +1,14 @@
+package com.larrykin343.studentgrademanagmentsystem;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.larrykin.studentgrademanagmentsystem {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.larrykin343.studentgrademanagmentsystem to javafx.fxml;
+    exports com.larrykin343.studentgrademanagmentsystem;
+}
