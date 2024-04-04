9eadbfccb85fb7df742829afc2e53fd9febb8a6c
kinuthia lawrence <kinuthialawrence343@gmail.com>
2024-04-04 18:36:30 +0300

    unwrapped clases in grade report

--- src/main/java/com/larrykin343/studentgrademanagmentsystem/Controller/GradeReportController.java
+++ src/main/java/com/larrykin343/studentgrademanagmentsystem/Controller/GradeReportController.java
@@ -2,12 +2,20 @@ package com.larrykin343.studentgrademanagmentsystem.Controller;
 
 import com.larrykin343.studentgrademanagmentsystem.Utils.EmailSender;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
 
 public class GradeReportController {
 
 
+    public Button closeGradeReportButton;
+
     public void exportResults(ActionEvent actionEvent) {
         EmailSender emailSender = new EmailSender();
         emailSender.sendStudentResultsByEmail();
+    }
+
+    public void closeWindow(ActionEvent event) {
+        closeGradeReportButton.getScene().getWindow().hide();
+
     }
 }
